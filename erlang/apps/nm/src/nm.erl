8d09e668eb7f3dba00e651532736385c1a602234
Mikael Pettersson <mikpelinux@gmail.com>
2020-03-01 21:55:37 +0100

    nm: fix dialyzer error

--- erlang/apps/nm/src/nm.erl
+++ erlang/apps/nm/src/nm.erl
@@ -1,7 +1,7 @@
 %%% -*- erlang-indent-level: 2 -*-
 %%%
 %%% 'nm' clone for pdp10-elf
-%%% Copyright (C) 2013-2019  Mikael Pettersson
+%%% Copyright (C) 2013-2020  Mikael Pettersson
 %%%
 %%% This file is part of pdp10-tools.
 %%%
@@ -42,6 +42,7 @@
 main(Argv) ->
   escript_runtime:start(fun main_/1, Argv).
 
+-spec main_([string()]) -> no_return().
 main_(Argv) ->
   %% NYI options:
   %% -a / --debug-syms
