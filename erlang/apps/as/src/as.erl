84f67a1ad90c7856e3cf18283a8adccd13f73c86
Mikael Pettersson <mikpelinux@gmail.com>
2020-03-03 23:27:59 +0100

    as: as: fix dialyzer error

--- erlang/apps/as/src/as.erl
+++ erlang/apps/as/src/as.erl
@@ -1,7 +1,7 @@
 %%% -*- erlang-indent-level: 2 -*-
 %%%
 %%% 'as' clone for pdp10-elf
-%%% Copyright (C) 2013-2019  Mikael Pettersson
+%%% Copyright (C) 2013-2020  Mikael Pettersson
 %%%
 %%% This file is part of pdp10-tools.
 %%%
@@ -26,6 +26,7 @@
 main(Argv) ->
   escript_runtime:start(fun main_/1, Argv).
 
+-spec main_([string()]) -> no_return().
 main_(Argv) ->
   case getopt:parse(Argv, "vo:",
                     [ {"version", no, version}
