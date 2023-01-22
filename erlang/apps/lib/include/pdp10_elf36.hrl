c0ae20c6e2e7bd928cddaf33e01e65e944c7f1a9
Mikael Pettersson <mikpelinux@gmail.com>
2023-01-22 16:35:18 +0100

    pdp10_elf36.hrl: sync with binutils-2.40 ELF definitions

--- erlang/apps/lib/include/pdp10_elf36.hrl
+++ erlang/apps/lib/include/pdp10_elf36.hrl
@@ -1,7 +1,7 @@
 %%% -*- erlang-indent-level: 2 -*-
 %%%
 %%% pdp10_elf36.hrl -- ELF definitions for PDP10
-%%% Copyright (C) 2013-2022  Mikael Pettersson
+%%% Copyright (C) 2013-2023  Mikael Pettersson
 %%%
 %%% This file is part of pdp10-tools.
 %%%
@@ -621,6 +621,8 @@
 
 %% Compression types.
 -define(ELFCOMPRESS_ZLIB,                 1).   % Compressed with zlib.
+-define(ELFCOMPRESS_ZSTD,                 2).   % Compressed with zstd
+                                                % (see http://www.zstandard.org).
 -define(ELFCOMPRESS_LOOS,       16#60000000).   % OS-specific semantics, lo
 -define(ELFCOMPRESS_HIOS,       16#6FFFFFFF).   % OS-specific semantics, hi
 -define(ELFCOMPRESS_LOPROC,     16#70000000).   % Processor-specific semantics, lo
@@ -1191,6 +1193,7 @@
 -define(PT_GNU_STACK,           (?PT_LOOS + 16#474e551)).       % Stack flags
 -define(PT_GNU_RELRO,           (?PT_LOOS + 16#474e552)).       % Read-only after relocation
 -define(PT_GNU_PROPERTY,        (?PT_LOOS + 16#474e553)).       % GNU property
+-define(PT_GNU_SFRAME,          (?PT_LOOS + 16#474e554)).       % SFrame unwind information
 
 %% OpenBSD segment types.
 -define(PT_OPENBSD_RANDOMIZE,   (?PT_LOOS + 16#5a3dbe6)).       % Fill with random data.
@@ -1587,6 +1590,8 @@
 -define(AT_FREEBSD_PS_STRINGS,  32).    % struct ps_strings.
 -define(AT_FREEBSD_FXRNG,       33).    % Pointer to root RNG seed version.
 -define(AT_FREEBSD_KPRELOAD,    34).    % Base of vdso.
+-define(AT_FREEBSD_USRSTACKBASE,35).    % Top of user stack.
+-define(AT_FREEBSD_USRSTACKLIM, 36).    % Grow limit of user stack.
 
 -define(AT_SUN_UID,             2000).  % Effective user ID.
 -define(AT_SUN_RUID,            2001).  % Real user ID.
